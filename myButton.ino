f595c65cba5f48f049a19276e449a1e632885fb4
Yosiya Hinosawa <stibium121@gmail.com>
2013-03-09 12:23:30 +0900

    created Switch class to handle tact switch handling.

--- myButton.ino
+++ myButton.ino
@@ -1,40 +1,78 @@
 
 const int LED = 13;
 const int BUTTON = 7;
-const int DUR = 500;
 
 const int STATE_ON = 1;
 const int STATE_OFF = 0;
 
-int val = LOW;
-int prevVal = LOW;
-int prevPrevVal = LOW;
-int prevPrevPrevVal = LOW;
-int state = 0;
+class Switch {
+private:
+  int button;
+  int val;
+  int valPrev;
+  int valPrevPrev;
+  int valPrevPrevPrev;
+
+public:
+  Switch(int);
+  boolean turnedOn(void);
+  void shiftValues(int);
+  boolean buttonIsHigh(void);
+  boolean buttonTurnedHigh(void);
+};
+
+Switch *swt;
+int state = STATE_OFF;
 
 void setup() {
   pinMode(LED, OUTPUT);
   pinMode(BUTTON, INPUT);
+  swt = new Switch(BUTTON);
 }
 
 void loop() {
-  val = digitalRead(BUTTON);
-  
-  if (val == HIGH && prevVal == LOW && prevPrevVal == LOW  && prevPrevPrevVal == LOW) {
-    delay(20);
-    val = digitalRead(BUTTON);
-    if (val == HIGH) {
-      state ^= 1;
-    }
+  if (swt->turnedOn()) {
+    state ^= 1;
   }
-  
+
   if (state == STATE_ON) {
     digitalWrite(LED, HIGH);
   } else {
     digitalWrite(LED, LOW);
   }
-  
-  prevPrevPrevVal = prevPrevVal;
-  prevPrevVal = prevVal;
-  prevVal = val;
+}
+
+Switch::Switch(int button) {
+  this->button = button;
+  this->valPrev = LOW;
+  this->valPrevPrev = LOW;
+  this->valPrevPrevPrev = LOW;
+}
+
+boolean Switch::turnedOn() {
+  boolean flag = false;
+  if (this->buttonTurnedHigh()) {
+    delay(20);
+    if (this->buttonIsHigh()) {
+      this->shiftValues(HIGH);
+      return true;
+    }
+  }
+
+  this->shiftValues(LOW);
+  return false;
+}
+
+void Switch::shiftValues(int val) {
+  this->valPrevPrevPrev = this->valPrevPrev;
+  this->valPrevPrev = this->valPrev;
+  this->valPrev = val;
+}
+
+boolean Switch::buttonIsHigh() {
+  return digitalRead(this->button);
+}
+
+boolean Switch::buttonTurnedHigh() {
+  return this->buttonIsHigh() && this->valPrev == LOW && this->valPrevPrev == LOW && this->valPrevPrevPrev == LOW;
 }
